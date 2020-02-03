377c7d58edcbfcfab72cb29ec3ce4cb87c8b41d8
unibitdev <57153156+unibitdev@users.noreply.github.com>
2020-02-03 13:00:57 -0600

    Add files via upload

---
+++ src/config/Ascii.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2020, The Universal Bit Project
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string windowsAsciiArt =
+ "\n  \n"                                                                              
+"   _/    _/  _/      _/  _/_/_/  _/_/_/    _/_/_/  _/_/_/_/_/  _/      _/    \n"
+"  _/    _/  _/_/    _/    _/    _/    _/    _/        _/        _/  _/       \n"
+" _/    _/  _/  _/  _/    _/    _/_/_/      _/        _/          _/          \n"
+"_/    _/  _/    _/_/    _/    _/    _/    _/        _/        _/  _/         \n"
+" _/_/    _/      _/  _/_/_/  _/_/_/    _/_/_/      _/      _/      _/        \n";
+                                   
+                           
+
+const std::string nonWindowsAsciiArt =
+  
+  
+ "\n  \n"                                                                              
+"   _/    _/  _/      _/  _/_/_/  _/_/_/    _/_/_/  _/_/_/_/_/  _/      _/    \n"
+"  _/    _/  _/_/    _/    _/    _/    _/    _/        _/        _/  _/       \n"
+" _/    _/  _/  _/  _/    _/    _/_/_/      _/        _/          _/          \n"
+"_/    _/  _/    _/_/    _/    _/    _/    _/        _/        _/  _/         \n"
+" _/_/    _/      _/  _/_/_/  _/_/_/    _/_/_/      _/      _/      _/        \n";
+                                      
+
+/* Windows has some characters it won't display in a terminal. If your ascii
+   art works fine on Windows and Linux terminals, just replace 'asciiArt' with
+   the art itself, and remove these two #ifdefs and above ascii arts */
+#ifdef _WIN32
+const std::string asciiArt = windowsAsciiArt;
+#else
+const std::string asciiArt = nonWindowsAsciiArt;
+#endif
